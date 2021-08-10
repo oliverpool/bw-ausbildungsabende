2ad47627f8b31ff0d3b444d646e3d8c0c2b1ca68
oliverpool <git@olivier.pfad.fr>
2021-08-10 14:07:42 +0200

    router: scroll behavior

--- src/router.ts
+++ src/router.ts
@@ -33,4 +33,11 @@ const routes: RouteRecordRaw[] = [
 export default createRouter({
   history: createWebHashHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
 })
