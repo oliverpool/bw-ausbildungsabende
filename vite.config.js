ec4a4180b07ba146cc5dbe96dacbb173b2d75495
oliverpool <git@olivier.pfad.fr>
2021-08-12 12:24:30 +0200

    working pwa

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,41 @@ import * as path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pkg from './package.json'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'safari-pinned-tab.svg'],
+      manifest: {
+        name: 'Anwesenheit - Ausbildungsabende - Bergwacht Bad Tölz',
+        short_name: 'Anwesenheit',
+        description: 'Anwesenheitsliste für die Ausbildungsabende der Bergwacht Bad Tölz',
+        theme_color: '#0088ce',
+        background_color: '#0088ce',
+        lang: 'de',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
