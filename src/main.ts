3a682c827e69300816cd0a52dd2f84c2a0bdfa3a
oliverpool <git@olivier.pfad.fr>
2021-08-14 09:39:06 +0200

    fix worker registration

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import { useRegisterSW } from 'virtual:pwa-register/vue'
+useRegisterSW()
 
 createApp(App).use(router).mount('#app')
