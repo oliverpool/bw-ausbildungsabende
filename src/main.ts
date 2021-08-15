0613bc3a8c613edadca6201f434159a351ae4d45
oliverpool <git@olivier.pfad.fr>
2021-08-15 10:50:53 +0200

    worker: periodic update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 import { useRegisterSW } from 'virtual:pwa-register/vue'
-useRegisterSW()
+useRegisterSW({
+  onRegistered(r) {
+    r &&
+      setInterval(() => {
+        r.update()
+      }, 24 * 60 * 60 * 1000)
+  },
+})
 
 createApp(App).use(router).mount('#app')
